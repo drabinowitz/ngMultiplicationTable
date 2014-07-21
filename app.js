715c9a33bdb446cef4ab6f0551609cf808b0cd2f
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-07-21 11:11:17 -0400

    finished app

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 angular.module('myApp',[])
-	.controller('MultiplicationCtrl',function($scope, $attrs){
+
+	.controller('displayCtrl',function($scope){
+
+		$scope.$on('displayData',function(event,data){
+
+			$scope.content = data;
+
+		});
+
+	})
+
+	.controller('MultiplicationCtrl',function($rootScope, $scope, $attrs){
 
 		function populateNumbers(upperLimit){
 
@@ -15,6 +26,12 @@ angular.module('myApp',[])
 
 		}
 
+		$scope.$watch('numberLimit',function(limit){
+
+			$scope.numbers = populateNumbers(limit);
+
+		});
+
 		$scope.numberLimit = $attrs.initialNumberLimit || 10;
 
 		$scope.numbers = populateNumbers($scope.numberLimit);
@@ -22,5 +39,35 @@ angular.module('myApp',[])
 		$scope.compute = function(a,b){
 			return a * b;
 		}
+
+		var activeFactorA;
+
+		var activeFactorB;
+
+		$scope.clearActiveFactors = function(){
+
+			activeFactorA = activeFactorB = null;
+
+		};
+
+		$scope.setActiveFactors = function(a,b){
+
+			activeFactorA = a;
+
+			activeFactorB = b;
+
+		};
+
+		$scope.matchesFactor = function(a,b){
+
+			return a == activeFactorA || b == activeFactorB;
+
+		};
+
+		$scope.setActiveNumber = function(number){
+
+			$rootScope.$broadcast('displayData', number);
+
+		};
 
 	});
